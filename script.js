daa8c0df0a5ec965ecd5c91a5c91ca09ee5968f8
bst04 <75524846+brunoooost@users.noreply.github.com>
2024-11-13 17:23:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Example of adding smooth scrolling behavior to the page
-document.querySelectorAll('a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
+//document.querySelectorAll('a').forEach(anchor => {
+//  anchor.addEventListener('click', function (e) {
+ //   e.preventDefault();
+   // document.querySelector(this.getAttribute('href')).scrollIntoView({
+     // behavior: 'smooth'
+//    });
+//  });
+// });
